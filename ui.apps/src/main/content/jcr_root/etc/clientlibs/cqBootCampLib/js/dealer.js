7ee0d1f8528a14300148fe123d198b1877133e1e
shivani <shivanig@intelligrape.com>
2015-04-09 16:56:03 +0530

    changed

--- ui.apps/src/main/content/jcr_root/etc/clientlibs/cqBootCampLib/js/dealer.js
+++ ui.apps/src/main/content/jcr_root/etc/clientlibs/cqBootCampLib/js/dealer.js
@@ -1,99 +1,79 @@
-$(document).ready(function () {
-    var path = $("#path").val();
-    $.ajax({
-        type: "GET",
-        url: "/bin/dealer",
-        dataType: "json",
-        data: { 'path': path      },
-        success: function (data) {
-            $('#state option:not(:first)').remove();
-            $.each(data.States, function (i, item) {
-                $("#state").append("<option value=" + item.id + ">" + item.name + "</option>");
-            });
-        },
-        error: function () {
-<<<<<<< Updated upstream
-
-=======
->>>>>>> Stashed changes
+    $(document).ready(function()
+        {  var path=$("#path").val();    
+            $.ajax({
+            type: "GET",
+            url: "/bin/dealer",
+            dataType: "json",
+                data: { 'path1':path      },
+            success:  function (data) {
+                $('#state option:not(:first)').remove();
+                $.each(data.States,function(i,item)
+                {
+                    $("#state").append("<option value="+item.id+">"+item.name+"</option>");});  },
+            error:function(){
             console.log("error");
-        }
-    });
-    $("#state").change(function () {
-        var path = $("#path").val();
-<<<<<<< Updated upstream
-        var stateError = $("#stateError").val();
-        var state = $('#state option:selected').val();
+            }
+        });
+            $( "#state" ).change(function() {
+              var path=$("#path").val();  
+                var stateError=$("#stateError").val();
+                var state= $('#state option:selected').val();
 
-=======
-        var state = $('#state option:selected').val();
->>>>>>> Stashed changes
-        if ($("#state").val() != "states") {
-            $.ajax({
-                type: "GET",
-                url: "/bin/dealer",
-                dataType: "json",
-                data: {'path': path,
-                    'state': state },
-                success: function (data) {
-                    $('#cities option:not(:first)').remove();
-                    $.each(data.States, function (i, item) {
-                        $("#cities").append("<option value=" + item.id + ">" + item.name + "</option>");
-                    });
-                },
-                error: function () {
-                    console.log("error");
-<<<<<<< Updated upstream
+                if ( $("#state").val()!="states")
+           {  $.ajax({
+                    type: "GET",
+                    url: "/bin/dealer",
+                    dataType: "json",
+                    data: {'path1':path ,
+                        'state':state },
+                    success:  function  (data) {
+                     $('#cities option:not(:first)').remove();
+                        $.each(data.States,function(i,item)
+                        {
+                            $("#cities").append("<option value="+item.id+">"+item.name+"</option>");
+                        });   }, 
 
-=======
->>>>>>> Stashed changes
-                }  });
-        }
-        else {
-            $('#cities option:not(:first)').remove();
-        }
-    });
-    $(".submit").click(function () {
-        var path = $("#path").val();
-<<<<<<< Updated upstream
-        var errorMessage = $("#stateError").val();
-=======
-        var errorMessage = $("#Error").val();
->>>>>>> Stashed changes
-        var state = $('#state option:selected').val();
-        var city = $('#cities option:selected').val();
-        if ($('#cities').val() != "cities") {
-            $.ajax({
-                type: "GET",
-                url: "/bin/dealer",
-                dataType: "json",
-                data: {'path': path,
-                    'state': state,
-                    'city': city},
-                success: function (data) {
-                    $("#message").text("");
-                    $('tbody tr:not(:first)').remove();
-                    var count = 1;
-                    $.each(data.States, function (i, item) {
-                        $("#table").append("<tr><td>" + count++ + "</td><td>" + item.dealerName + "</td><td>" + item.address + "</td><td>" + item.contact + "</td><td>" + item.email + "</td></tr>");
-                    });
-                },
-<<<<<<< Updated upstream
-                error: function () {
-                    $('tbody tr:not(:first)').remove();
-                }  });
-=======
+                    error:function (){
+                         console.log("error");
 
-                error: function () {
+                    }  }); }
+                else
+                {
+                     $('#cities option:not(:first)').remove();
+                }});
+            $( ".submit" ).click(function() {
+                var path=$("#path").val(); 
+                var errorMessage = $("#Error").val();
+                var state = $('#state option:selected').val();
+                var city= $('#cities option:selected').val();
+
+
+                 if ( $('#cities').val()!="cities")
+                {$.ajax({
+                    type: "GET",
+                    url: "/bin/dealer",
+                    dataType: "json",
+                     data: {'path1':path,
+                            'state':state,
+                            'city':city},
+                    success:  function  (data) {
+                         $("#message").text("");
+                        $('tbody tr:not(:first)').remove();
+                        var count=1;
+                        $.each(data.States,function(i,item)
+                        {
+                            $("#table").append("<tr><td>"+ count++ +"</td><td>"+item.dealerName+"</td><td>"+item.address+"</            td><td>"+item.contact+"</td><td>"+item.email+"</td></tr>");
+                        });  },
+
+                    error:function (){
+                        $('tbody tr:not(:first)').remove();
+}});}
+              else
+                {
+                   $("#message").text(errorMessage);
                     $('tbody tr:not(:first)').remove();
-                }              });
->>>>>>> Stashed changes
-        }
-        else {
-            $("#message").text(errorMessage);
-            $('tbody tr:not(:first)').remove();
-        }
-    });
-});
+                }
+            });
+        });
 
 
